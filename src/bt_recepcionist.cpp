ac7904c9ebf51a2c3d5112bf8c0a7d8b081d1b14
GuilleAQ <guillermoalque8@gmail.com>
2023-05-10 00:53:40 +0200

    arreglo

--- src/bt_recepcionist.cpp
+++ src/bt_recepcionist.cpp
@@ -24,6 +24,9 @@
 #include "ament_index_cpp/get_package_share_directory.hpp"
 
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 
 #include "rclcpp/rclcpp.hpp"
